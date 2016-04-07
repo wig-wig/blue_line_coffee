4d63d3e2ab704cc23c7b81d473f8ea6790848e28
Sumeet Jain <sumeet@sumeetjain.com>
2016-04-07 13:54:05 -0500

    Indentation

--- gruntfile.js
+++ gruntfile.js
@@ -1,159 +1,159 @@
 module.exports = function(grunt) {
-grunt.initConfig({
+  grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
-        dist: {
-            files: {
-                "dev/css/styles.css": "dev/scss/styles.scss"
-            }
+      dist: {
+        files: {
+          "dev/css/styles.css": "dev/scss/styles.scss"
         }
+      }
     },
     watch: {
-        livereload: {
-            options: { livereload: true },
-            files: ['dev/css/*.css', 'dev/js/site/*.js', 'dev/**/*.html']
-        },
-        sass: {
-            files: 'dev/scss/*.scss',
-            tasks: ['sass']
-        },
+      livereload: {
+        options: { livereload: true },
+        files: ['dev/css/*.css', 'dev/js/site/*.js', 'dev/**/*.html']
+      },
+      sass: {
+        files: 'dev/scss/*.scss',
+        tasks: ['sass']
+      },
     },
     connect: {
-        server: {
-            options: {
-                port: 8889,
-                hostname: '*',
-                livereload: true,
-                open: true,
-                base: 'dev'
-            }
-        },
-        buildTest: {
-            options: {
-                port: 8889,
-                hosthame: '*',
-                open: true,
-                base: 'build',
-                keepalive: true
-            }
+      server: {
+        options: {
+          port: 8889,
+          hostname: '*',
+          livereload: true,
+          open: true,
+          base: 'dev'
         }
+      },
+      buildTest: {
+        options: {
+          port: 8889,
+          hosthame: '*',
+          open: true,
+          base: 'build',
+          keepalive: true
+        }
+      }
     },
-        copy: {
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dev',
-                        src: '**',
-                        dest: 'build/'
-                    }
-                ]
-            }
-        },
-        cssmin: {
-            combine: {
-                files: {
-                    'build/css/styles.css': 'dev/css/styles.css'
-                }
-            }
-        },
-        uglify: {
-            my_target: {
-                files: [{
-                    expand: true,
-                    cwd: 'dev/js/site',
-                    src: '**/*.js',
-                    dest: 'build/js/site'
-                }]
-            }
-        },
-        clean: {
-            build: ['build'],
-            cleanup: [
-                'build/css/*.*.map',
-                'build/img/uncompressed',
-                'build/scss'
-            ]
-        },
-        cacheBust: {
-            options: {
-                encoding: 'utf8',
-                algorithm: 'md5',
-                length: 8,
-                baseDir: 'build',
-                rename: false,
-                ignorePatterns: ['img']
-            },
-            assets: {
-                files: [{
-                    src: ['build/*.html'],
-                    dest: 'build/'
-                }]
-            }
-        },
-        compress: {
-            build: {
-                options: {
-                    archive: 'build.' + dateHash() + '.zip'
-                },
-                files: [
-                    {
-                        src: ['build/**'],
-                        dest: '/'
-                    }
-                ]
-            }
+    copy: {
+      main: {
+        files: [
+        {
+          expand: true,
+          cwd: 'dev',
+          src: '**',
+          dest: 'build/'
         }
-    });
+        ]
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'build/css/styles.css': 'dev/css/styles.css'
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'dev/js/site',
+          src: '**/*.js',
+          dest: 'build/js/site'
+        }]
+      }
+    },
+    clean: {
+      build: ['build'],
+      cleanup: [
+      'build/css/*.*.map',
+      'build/img/uncompressed',
+      'build/scss'
+      ]
+    },
+    cacheBust: {
+      options: {
+        encoding: 'utf8',
+        algorithm: 'md5',
+        length: 8,
+        baseDir: 'build',
+        rename: false,
+        ignorePatterns: ['img']
+      },
+      assets: {
+        files: [{
+          src: ['build/*.html'],
+          dest: 'build/'
+        }]
+      }
+    },
+    compress: {
+      build: {
+        options: {
+          archive: 'build.' + dateHash() + '.zip'
+        },
+        files: [
+        {
+          src: ['build/**'],
+          dest: '/'
+        }
+        ]
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-cache-bust');
-    grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-cache-bust');
+  grunt.loadNpmTasks('grunt-contrib-compress');
 
-grunt.registerTask('server', [
+  grunt.registerTask('server', [
     'sass',
     'connect:server',
     'watch'
-]);
-
-    grunt.registerTask('buildTest', [
-        'connect:buildTest'
     ]);
 
-    grunt.registerTask('build', [
-        'sass',
-        'clean:build',
-        'copy',
-        'cssmin',
-        'uglify',
-        'clean:cleanup',
-        'cacheBust',
-        'compress'
+  grunt.registerTask('buildTest', [
+    'connect:buildTest'
+    ]);
+
+  grunt.registerTask('build', [
+    'sass',
+    'clean:build',
+    'copy',
+    'cssmin',
+    'uglify',
+    'clean:cleanup',
+    'cacheBust',
+    'compress'
     ]);
 };
 
 //for zip creation
 var dateHash = function() {
-    var today = new Date();
-    var dd = today.getDate();
+  var today = new Date();
+  var dd = today.getDate();
     var mm = today.getMonth()+1; //January is 0!
     var yyyy = today.getFullYear();
 
     if(dd<10) {
-        dd='0'+dd
+      dd='0'+dd
     }
 
     if(mm<10) {
-        mm='0'+mm
+      mm='0'+mm
     }
 
     var date = yyyy + '-' + mm + '-' + dd + '.' + today.getTime();
     return date;
-}
+  }
